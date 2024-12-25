081a83f12741734bc141145ff35bf4eb6d4c44a3
Chandler Boone Ferry <ChandlerFerry@users.noreply.github.com>
2024-12-25 14:26:38 +0000

    chore: extend profiles from a base

--- ProfileContent.cs
+++ ProfileContent.cs
@@ -8,17 +8,17 @@ public class ProfileContent
     public Dictionary<string, float> AfflictionWeights { get; set; }
     public Dictionary<string, float> RewardWeights { get; set; }
 
-    public static ProfileContent CreateDefaultProfile()
+    private static ProfileContent CreateBaseProfile()
     {
-        var profile = new ProfileContent
+        return new ProfileContent
         {
             RoomTypeWeights = new()
             {
-                ["Gauntlet"] = -1000,
-                ["Chalice"] = 0,
-                ["Hourglass"] = 0,
-                ["Escape"] = 100,
-                ["Ritual"] = 0,
+                ["Gauntlet"] = -1000,    // Annoying
+                ["Hourglass"] = -250,    // Dense mobs but manageable with defenses
+                ["Chalice"] = 0,         // Neutral
+                ["Ritual"] = 0,          // Neutral
+                ["Escape"] = 100,        // Safe, controlled environment
             },
             AfflictionWeights = new()
             {
@@ -42,7 +42,7 @@ public class ProfileContent
                 ["Iron Manacles"] = 0, // You have no [Evasion]
                 ["Glass Shard"] = 0, // The next [Boons|Boon] you gain is converted into a random Minor [Afflictions|Affliction]
                 ["Tradition's Demand"] = 0, // The Merchant only offers one choice
-                ["Worn Sandals"] = 0, // 25% reduced Movement Speed
+                ["Worn Sandals"] = -250, // 25% reduced Movement Speed
                 ["Fiendish Wings"] = 0, // Monsters' Action Speed cannot be slowed below base
                 ["Weakened Flesh"] = 0, // 25% less Maximum [Honour]
                 ["Rusted Mallet"] = 0, // Monsters always [Knockback]
@@ -69,7 +69,7 @@ public class ProfileContent
                 ["Exhausted Wells"] = 0, // Chests no longer grant [SacredWater|Sacred Water]
                 ["Gate Toll"] = 0, // Lose 30 [SacredWater|Sacred Water] on room completion
                 ["Leaking Waterskin"] = 0, // Lose 20 [SacredWater|Sacred Water] when you take Damage from an Enemy [HitDamage|Hit]
-                ["Rapid Quicksand"] = 0, // Traps are faster
+                ["Rapid Quicksand"] = -300, // Traps are faster
                 ["Blunt Sword"] = 0, // You and your Minions deal 40% less Damage
                 ["Low Rivers"] = 0, // 50% less [SacredWater|Sacred Water] found
                 ["Dark Pit"] = 0, // Traps deal 100% increased Damage
@@ -94,95 +94,25 @@ public class ProfileContent
                 ["Merchant"] = 0
             }
         };
+    }
+
+    public static ProfileContent CreateDefaultProfile()
+    {
+        var profile = CreateBaseProfile();
+        
         return profile;
     }
 
     public static ProfileContent CreateNoHitProfile()
     {
-        var profile = new ProfileContent
-        {
-            RoomTypeWeights = new()
-            {
-                ["Gauntlet"] = -500,
-                ["Chalice"] = 0,
-                ["Hourglass"] = -750,
-                ["Escape"] = 100,
-                ["Ritual"] = 0,
-            },
-            AfflictionWeights = new()
-            {
-                // Major Afflictions for no-hit
-                ["Corrosive Concoction"] = 0, // You have no [Defences]
-                ["Glass Shard"] = 0, // The next [Boons|Boon] you gain is converted into a random Minor [Afflictions|Affliction]
-                ["Chiselled Stone"] = 0, // Monsters [Petrify] on Hit
-                ["Orb of Negation"] = 0, // Non-[ItemRarity|Unique] [Relic|Relics] have no Effect
-                ["Unquenched Thirst"] = 0, // You cannot gain [SacredWater|Sacred Water]
-                ["Unassuming Brick"] = 0, // You cannot gain any more [Boons]
-                ["Orbala's Leathers"] = 0, // Unknown
-                ["Ghastly Scythe"] = 0, // Losing [Honour] ends the Trial (removed after 3 rooms)
-                ["Veiled Sight"] = 0, // Rooms are unknown on the Trial Map
-                ["Branded Balbalakh"] = 0, // Cannot restore [Honour]
-                ["Death Toll"] = -1000000, // Take {0} [Physical] Damage after completing the next Room || ? Monsters no longer drop [SacredWater|Sacred Water]
-                ["Deadly Snare"] = 0, // Traps deal Triple Damage
+        var profile = CreateBaseProfile();
 
-                // Minor Afflictions for no-hit
-                ["Shattered Shield"] = 0, // You have no [EnergyShield|Energy Shield]
-                ["Sharpened Arrowhead"] = 0, // You have no [Armour]
-                ["Iron Manacles"] = 0, // You have no [Evasion]
-                ["Glass Shard"] = 0, // The next [Boons|Boon] you gain is converted into a random Minor [Afflictions|Affliction]
-                ["Tradition's Demand"] = 0, // The Merchant only offers one choice
-                ["Worn Sandals"] = 0, // 25% reduced Movement Speed
-                ["Fiendish Wings"] = 0, // Monsters' Action Speed cannot be slowed below base
-                ["Weakened Flesh"] = 0, // 25% less Maximum [Honour]
-                ["Rusted Mallet"] = 0, // Monsters always [Knockback]
-                ["Untouchable"] = 0, // You are [Curse|Cursed] with [Enfeeble]
-                ["Chains of Binding"] = 0, // Monsters inflict [BindingChains|Binding Chains] on [HitDamage|Hit]
-                ["Costly Aid"] = 0, // Gain a random Minor [Afflictions|Affliction] when you venerate a Maraketh Shrine
-                ["Myriad Aspersions"] = 0, // When you gain an [Afflictions|Affliction], gain an additional random Minor [Afflictions|Affliction]
-                ["Season of Famine"] = 0, // The Merchant offers 50% fewer choices
-                ["Forgotten Traditions"] = 0, // 50% reduced Effect of your Non-[ItemRarity|Unique] [Relic|Relics]
-                ["Haemorrhage"] = 0, // You cannot restore [Honour] (removed after killing the next Boss)
-                ["Deceptive Mirror"] = 0, // You are not always taken to the room you select
-                ["Dishonoured Tattoo"] = 0, // 100% increased Damage Taken while on [LowLife|Low Life]
-                ["Tattered Blindfold"] = 0, // 90% reduced Light Radius
-                ["Spiked Exit"] = -1000000, // Take {0} [Physical] Damage on Room Completion
-                ["Purple Smoke"] = 0, // [Afflictions] are unknown on the Trial Map
-                ["Golden Smoke"] = 0, // Rewards are unknown on the Trial Map
-                ["Red Smoke"] = 0, // Room types are unknown on the Trial Map
-                ["Winter Drought"] = 0, // Lose all [SacredWater|Sacred Water] on floor completion
-                ["Trade Tariff"] = 0, // 50% increased Merchant prices
-                ["Black Smoke"] = 0, // You can see one fewer room ahead on the Trial Map
-                ["Suspected Sympathiser"] = 0, // 50% reduced [Honour] restored
-                ["Hungry Fangs"] = 0, // Monsters remove 5% of your Life, Mana and [EnergyShield|Energy Shield] on [HitDamage|Hit]
-                ["Spiked Shell"] = 0, // Monsters have 50% increased Maximum Life
-                ["Exhausted Wells"] = 0, // Chests no longer grant [SacredWater|Sacred Water]
-                ["Gate Toll"] = 0, // Lose 30 [SacredWater|Sacred Water] on room completion
-                ["Leaking Waterskin"] = 0, // Lose 20 [SacredWater|Sacred Water] when you take Damage from an Enemy [HitDamage|Hit]
-                ["Rapid Quicksand"] = 0, // Traps are faster
-                ["Blunt Sword"] = 0, // You and your Minions deal 40% less Damage
-                ["Low Rivers"] = 0, // 50% less [SacredWater|Sacred Water] found
-                ["Dark Pit"] = 0, // Traps deal 100% increased Damage
-                ["Honed Claws"] = 0, // Monsters deal 30% more Damage
-            },
-            RewardWeights = new()
-            {
-                ["Gold Key"] = 0,
-                ["Silver Key"] = 0,
-                ["Bronze Key"] = 0,
-                ["Large Fountain"] = 0,
-                ["Fountain"] = 0,
-                ["Pledge to Kochai"] = 0,
-                ["Honour Halani"] = 0,
-                ["Honour Ahkeli"] = 0,
-                ["Honour Orbala"] = 0,
-                ["Honour Galai"] = 0,
-                ["Honour Tabana"] = 0,
-                ["Golden Cache"] = 0,
-                ["Silver Cache"] = 0,
-                ["Bronze Cache"] = 0,
-                ["Merchant"] = 0
-            }
-        };
+        profile.RoomTypeWeights["Gauntlet"] = -250;     // Predictable traps
+        profile.RoomTypeWeights["Hourglass"] = -750;    // Dangerous mob density
+
+        profile.AfflictionWeights["Death Toll"] = -1000000; // Run-Ending
+        profile.AfflictionWeights["Spiked Exit"] = -1000000; // Run-Ending
+
         return profile;
     }
 
